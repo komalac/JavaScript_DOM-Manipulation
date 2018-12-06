c4a831cd5a2aef82cc5bd42edfe5101e519a6711
Komala <komala.chandran@capgemini.com>
2018-12-05 21:43:36 -0500

    Completed Step1

--- static/js/app.js
+++ static/js/app.js
@@ -2,7 +2,7 @@
 var tableID = d3.select('table');
 var tableBodyID = tableID.select("tbody");
 
-var submitBtn = d3.select("#submit");
+
 
 // YOUR CODE HERE!
 data.forEach((ddisplay) => {
@@ -13,12 +13,17 @@ data.forEach((ddisplay) => {
   });
 });
 
+var submitBtn = d3.select("#filter-btn");
+
 submitBtn.on("click",function(){
 
-    var inputBox = d3.select("datetime");
-    var inputDate = inputBox.property("value");
-    var filterData = data.filter(alien => alien.datetime === inputDate)    ;
+    // Prevent the page from refreshing
+    d3.event.preventDefault();    
 
+    var inputDate = d3.select("#datetime").property("value");
+    var filterData = data.filter(alien => alien.datetime === inputDate);
+
+    tableBodyID.html("")
     filterData.forEach((ddisplay) => {
     var row = tableBodyID.append("tr");
     Object.entries(ddisplay).forEach(([key, value]) => {
