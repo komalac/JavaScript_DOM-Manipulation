78516c654ef91171f718d41e5054685be6295a2e
Komala <komala.chandran@capgemini.com>
2018-12-07 22:07:03 -0500

    Updated to handle empty input

--- static/js/app.js
+++ static/js/app.js
@@ -2,16 +2,8 @@
 var tableID = d3.select('table');
 var tableBodyID = tableID.select("tbody");
 
-
-
-// YOUR CODE HERE!
-data.forEach((ddisplay) => {
-  var row = tableBodyID.append("tr");
-  Object.entries(ddisplay).forEach(([key, value]) => {
-    var cell = row.append("td");
-    cell.text(value);
-  });
-});
+// populate data by default
+displaydata(data)
 
 var submitBtn = d3.select("#filter-btn");
 
@@ -21,15 +13,26 @@ submitBtn.on("click",function(){
     d3.event.preventDefault();    
 
     var inputDate = d3.select("#datetime").property("value");
-    var filterData = data.filter(alien => alien.datetime === inputDate);
-
-    tableBodyID.html("")
-    filterData.forEach((ddisplay) => {
-    var row = tableBodyID.append("tr");
-    Object.entries(ddisplay).forEach(([key, value]) => {
-        var cell = row.append("td");
-        cell.text(value);
-    });
-    });
+    
+    // If the input box is empty, display all data, else filter by date
+    if (inputDate === ""){
+      displaydata(data)
+    }
+    else{
+      var filterData = data.filter(alien => alien.datetime === inputDate);
+      displaydata(filterData)
+    }     
+    
 })
 
+function displaydata(displaydata)
+{
+  tableBodyID.html("")
+  displaydata.forEach((ddisplay) => {
+  var row = tableBodyID.append("tr");
+  Object.entries(ddisplay).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+  });
+  });
+}
