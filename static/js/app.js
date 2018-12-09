79d1d3566666a46f5b3182ff3bd7b3ce9eae7eea
Komala <komala.chandran@capgemini.com>
2018-12-08 23:40:17 -0500

    Complete

--- static/js/app.js
+++ static/js/app.js
@@ -2,29 +2,62 @@
 var tableID = d3.select('table');
 var tableBodyID = tableID.select("tbody");
 
-// populate data by default
+// populate filters & data by default
 displaydata(data)
+populatedropdown()
+
 
 var submitBtn = d3.select("#filter-btn");
 
+// onclick submit button
 submitBtn.on("click",function(){
 
     // Prevent the page from refreshing
     d3.event.preventDefault();    
 
+    // getting input values from all filter field
     var inputDate = d3.select("#datetime").property("value");
-    
-    // If the input box is empty, display all data, else filter by date
-    if (inputDate === ""){
-      displaydata(data)
+    var inputcity = d3.select("#selcity").property("value");
+    var inputstate = d3.select("#selstate").property("value");
+    var inputcountry = d3.select("#selcountry").property("value");
+    var inputshape = d3.select("#selshape").property("value");
+
+    var arrfilter = []
+    filterData = data
+
+    // If any value is given, data is filtered up on the filtered set
+    if (inputDate != ""){      
+      var filterData = filterData.filter(alien => alien.datetime === inputDate);      
     }
-    else{
-      var filterData = data.filter(alien => alien.datetime === inputDate);
-      displaydata(filterData)
-    }     
-    
+
+    if (inputcity != ""){
+      var filterData = filterData.filter(alien => alien.city === inputcity); 
+    }
+
+    if (inputstate != ""){
+      var filterData = filterData.filter(alien => alien.state === inputstate); 
+    }
+
+    if (inputcountry != ""){
+      var filterData = filterData.filter(alien => alien.country === inputcountry); 
+    }
+
+    if (inputshape != ""){
+      var filterData = filterData.filter(alien => alien.shape === inputshape); 
+    }
+
+    // Display table with filtered data
+    displaydata(filterData)
+});
+  
+
+var resetBtn = d3.select("#reset-btn");
+// onclick function for Reset button, calls displaydata function
+resetBtn.on("click",function(){
+  displaydata(data)  
 })
 
+// Function to display data, populate table
 function displaydata(displaydata)
 {
   tableBodyID.html("")
@@ -35,4 +68,54 @@ function displaydata(displaydata)
       cell.text(value);
   });
   });
+}
+
+// Populate list with unique valules from the dataset
+function populatelist(darray, elementid){
+  
+  var filteredArray = [...new Set(darray)];
+  
+
+  var sel = document.getElementById(elementid);
+  
+  for(var i = 0; i < filteredArray.length; i++) {
+      var opt = document.createElement('option');
+      opt.innerHTML = filteredArray[i];
+      opt.value = filteredArray[i];
+      sel.appendChild(opt);
+  }
+}
+
+function populatedropdown(){
+  var citys = data.map(function(cityls) {
+    return cityls.city;
+  });
+  populatelist(citys, "selcity")
+  
+  
+  var states = data.map(function(statels) {
+    return statels.state;
+  });
+  populatelist(states, "selstate")
+  
+  
+  var countrys = data.map(function(countryls) {
+    return countryls.country;
+  });
+  populatelist(countrys, "selcountry")
+  
+  var shapes = data.map(function(shapels) {
+    return shapels.shape;
+  });
+  populatelist(shapes, "selshape")
+}
+
+function multiFilter(array, filters) {
+  const filterKeys = Object.keys(filters);
+  console.log(filterKeys);
+  // filters all elements passing the criteria
+  return array.filter((item) => {
+    // dynamically validate all filter criteria
+    return filterKeys.every(key => !!~filters[key].indexOf(item[key]));
+  })
 }
